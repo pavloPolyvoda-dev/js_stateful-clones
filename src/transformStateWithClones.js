0ddca7f7f27ecfef463ad43a09fc58e768fe8aca
Pavlo Polyvoda <pvlpeapod@gmail.com>
2026-01-12 13:41:11 +0200

    Changed creating copy of state before each itteration

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,36 +8,32 @@
  */
 
 function transformStateWithClones(state, actions) {
-  const currentState = { ...state };
   const stateHistory = [];
+  let prevState = state;
 
   for (const action of actions) {
-    const prevState = Object.assign(
-      currentState,
-      stateHistory[stateHistory.length - 1],
-    );
+    const stateCopy = { ...prevState };
 
     switch (action.type) {
       case 'addProperties':
-        Object.assign(prevState, action.extraData);
+        Object.assign(stateCopy, action.extraData);
         break;
 
       case 'removeProperties':
         for (const key of action.keysToRemove) {
-          delete prevState[key];
+          delete stateCopy[key];
         }
         break;
 
       case 'clear':
-        for (const elem of Object.keys(prevState)) {
-          delete prevState[elem];
+        for (const elem of Object.keys(stateCopy)) {
+          delete stateCopy[elem];
         }
         break;
     }
 
-    const cloneState = Object.assign({}, prevState);
-
-    stateHistory.push(cloneState);
+    stateHistory.push(stateCopy);
+    prevState = stateCopy;
   }
 
   return stateHistory;
